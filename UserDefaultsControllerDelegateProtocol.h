6de273d33480daab4a2fafda779878901f8ca5cc
Christian Schulze <christian.schulze@mni.thm.de>
2014-02-22 09:30:11 +0100

    Added documentation

--- UserDefaultsControllerDelegateProtocol.h
+++ UserDefaultsControllerDelegateProtocol.h
@@ -8,9 +8,25 @@
 
 #import <Foundation/Foundation.h>
 
+/*!
+ @header DisplayData Callback Protocol
+ This is the minimum of selectors to respond to asure proper communication between
+ the DataController and the data display delegate that handles the visual presentation.
+ @author Christian Schulze
+ @copyright Christian Schulze, andinfinity
+ @version 0.1
+ @updated 21.02.14
+ */
 @protocol UserDefaultsControllerDelegateProtocol <NSObject>
 
+/*!
+ @abstract At which refresh rate data should be retrieved.
+ */
 - (double)dataRefreshRate;
+
+/*!
+ @abstract Whether displaying of the data be animated or not.
+ */
 - (BOOL)animateVisualRepresentation;
 
 @end
