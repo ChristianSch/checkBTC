45bb7fea98cf11b4731c2c905cb2a31cde342f8f
Christian Schulze <christian.schulze@mni.thm.de>
2014-02-23 10:52:34 +0100

    + inserted backup code from appDelegate + fixed saving of new user defaults

--- PreferencesWindowController.m
+++ PreferencesWindowController.m
@@ -63,12 +63,6 @@
 
 - (IBAction)savePrefs:(id)sender
 {
-	/*
-	 TODO:
-	 * act on selector call of nib button "save". this should not be done in appDelegate!
-	 * save defaults
-	 */
-	
 	if (userDefaultsDelegate != nil)
 	{
 		NSString *currency = [[[self currencies] selectedItem] title];
@@ -81,7 +75,7 @@
 		[dict setValue:rRate forKey:@"refreshRate"];
 		[dict setValue:[[NSNumber alloc] initWithBool:[self startAtLogin]] forKey:@"startAtLogin"];
 		
-		NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+		[userDefaultsDelegate setUserDefaultsWithDict:dict];
 		
 	} else {
 		NSLog(@"No user defaults delegate! No settings saved.");
@@ -91,4 +85,32 @@
 	[[self window] orderOut:self];
 }
 
+// just backup code from appDelegate. ignore
+- (void)savePreferences:(NSNotification *)notif
+{
+	BOOL changed = NO;
+	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+	NSString *curr = [notif userInfo][@"currency"];
+	NSString *rate = [notif userInfo][@"refreshRate"];
+	BOOL startAtLogin = [[[notif userInfo] objectForKey:@"startAtLogin"] boolValue];
+	
+	if (![curr isEqualToString:[defaults stringForKey:@"currency"]]) {
+		[defaults removeObjectForKey:@"currency"];
+		[defaults setObject:curr forKey:@"currency"];
+		
+		changed = YES;
+		if (debug) NSLog(@"updated currency: %@", curr);
+		
+	} else if ([rate doubleValue] != [[defaults objectForKey:@"refreshRate"] doubleValue]) {
+		[defaults removeObjectForKey:@"refreshRate"];
+		[defaults setObject:rate forKey:@"refreshRate"];
+		
+		changed = YES;
+		if (debug) NSLog(@"updated rate: %f", [rate doubleValue]);
+		
+	} else if (startAtLogin) {
+		[defaults setBool:YES forKey:@"startAtLogin"];
+		if (debug) NSLog(@"start at login");
+	}
+}
 @end
