04f66166ed2132f7b7adbbf908a59511ee9011dd
Christian Schulze <christian.schulze@mni.thm.de>
2014-02-23 10:33:29 +0100

    + improved documentation + added delegation of setting and getting of user defaults (no notifications anymore)

--- PreferencesWindowController.h
+++ PreferencesWindowController.h
@@ -7,19 +7,36 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "UserDefaultsControllerDelegateProtocol.h"
 
+/*!
+ @header PreferencesWindowController
+ This controller handles the preferences window nib
+ @author Christian Schulze
+ @copyright Christian Schulze, andinfinity
+ @version 0.4
+ @updated 23.02.14
+ */
 @interface PreferencesWindowController : NSWindowController
 {
 	BOOL debug;
 	__weak NSPopUpButton *_currencies;
 	__weak NSTextField *_refreshRate;
+	id<UserDefaultsControllerDelegateProtocol> userDefaultsDelegate;
 }
 
 /*!
- @abstract Act on pushing the save button by making up a notification savePreferences with
+ @abstract Act on pushing the save button by making up a notification
+ savePreferences with
  the given input saved in the userInfo dictionary
  */
 - (IBAction) savePrefs:(id)sender;
+
+/*!
+ @abstract Set the delegate to handle changes made to the user defaults
+ @param delegate handles the changes
+ */
+- (void)setUserDefaultsDelegate:(id<UserDefaultsControllerDelegateProtocol>)delegate;
 
 @property (weak) IBOutlet NSPopUpButton *currencies;
 @property (weak) IBOutlet NSTextField *refreshRate;
