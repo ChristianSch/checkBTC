64996f3d27dd2aaaedf03c673c514e0f0f09252d
Christian Schulze <christian.schulze@mni.thm.de>
2014-02-23 10:51:20 +0100

    + reworked to conform to the dataSourceProtocol

--- MtgoxAPI.h
+++ MtgoxAPI.h
@@ -2,53 +2,20 @@
 //  MtgoxAPI.h
 //  CheckBTC
 //
-//  Created by X on 15.12.13.
-//  Copyright (c) 2013 X. All rights reserved.
+//  Created by Christian Schulze on 15.12.13.
+//  Copyright (c) 2013 Christian Schulze. All rights reserved.
 //
 
 #define BASE_URL @"https://data.mtgox.com/api/2/"
 #define USER_AGENT @"CheckBTC MtGox API"
 
 #import <Foundation/Foundation.h>
+#import "DataSourceProtocol.h"
 
-@interface MtgoxAPI : NSObject
+@interface MtgoxAPI : NSObject <DataSourceProtocol>
+{
+	NSDictionary *json;
+}
 
 extern NSDictionary *const currencies;
-
-/*!
- @abstract Returns the base URL for all MtGox Api calls (in dependency of a currency supported by the APi)
- @param curr The currency to set
- @return the requested URL for API calls
- */
-+ (NSString *) getBaseURLForCurrency:(NSString *)curr;
-
-/*!
- @abstract Gets the JSON data available at the URL
- @param url URL to retrieve the JSON from
- @return the requested JSON data as a NSDictionary
- */
-+ (NSDictionary *) getJSONFromURL:(NSURL *)url;
-
-/*!
- @abstract Returns full ticker data for a given currency.
- @param curr Currency to request data for
- @return Key-Value pairs with the data
- @see https://bitbucket.org/nitrous/mtgox-api/overview#markdown-header-requests
- */
-+ (NSDictionary *) getTickerForCurrency:(NSString *)curr;
-
-/*!
- @abstract Gets the average price of one Bitcoin for a given currency.
- @param curr Currency to request data for
- @return average exchange rate
- */
-+ (NSNumber *) getAvgForCurrency:(NSString *)curr;
-
-/*!
- @abstract Gets the (unicode) symbol of the given currency
- @param curr Currency to return the symbol for
- @return the requested symbol
- @see https://bitbucket.org/nitrous/mtgox-api/overview#markdown-header-currencies
- */
-+ (NSString *) getCurrencySymbol:(NSString *)curr;
 @end
--- MtgoxAPI.m
+++ MtgoxAPI.m
@@ -2,8 +2,8 @@
 //  MtgoxAPI.m
 //  CheckBTC
 //
-//  Created by X on 15.12.13.
-//  Copyright (c) 2013 X. All rights reserved.
+//  Created by Christian Schulze on 15.12.13.
+//  Copyright (c) 2013 Christian Schulze. All rights reserved.
 //
 
 #import "MtgoxAPI.h"
@@ -12,114 +12,98 @@ NSDictionary *const currencies = nil;
 
 @implementation MtgoxAPI
 
+- (id) init
+{
+	self = [super init];
+	
+	if (self) {
+		json = nil;
+	}
+	
+	return self;
+}
+
 #pragma mark - Data setup
 
-+ (NSString *) getBaseURLForCurrency:(NSString *)curr
++ (NSString *) getBaseURLForCurrency:(NSString *)currency
 {
 	/* set up the string consisting of the given currency and BTC,
 	 i.e. BTCEUR or BTCUSD */
-	NSString *currencyPair = [@"BTC" stringByAppendingString:curr];
+	NSString *currencyPair = [@"BTC" stringByAppendingString:currency];
 	
 	/* set up the url, i.e. http://data.mtgox.com/api/2/BTCEUR/money/ticker */
 	NSString *url = [BASE_URL stringByAppendingString:currencyPair];
 	return url;
 }
 
-#pragma mark - Data retrieval
-
-+ (NSDictionary *) getJSONFromURL:(NSURL *)url
+#pragma mark - protocol implementations
+- (void)handleData:(NSData*)data
 {
-	NSData *data = [[NSData alloc]initWithContentsOfURL:url];
-	
-	if (data != nil) {
-		// now we'll parse our data using NSJSONSerialization
-		id myJSON = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
-		NSDictionary *results = myJSON;
-		
-		return results;
-	}
-	
-	NSLog(@"no json returned");
-	/*  no data returned. maybe the connection is down? */
-	return nil;
-	
+	self->json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
 }
 
-# pragma mark - Access to processed Data
-+ (NSDictionary *) getTickerForCurrency:(NSString *)curr
+- (NSNumber*)getAvgForCurrency:(NSString*)currency
 {
-	/* set up the url, i.e. http://data.mtgox.com/api/2/BTCEUR/money/ticker */
-	NSString *url = [[self getBaseURLForCurrency:curr] stringByAppendingString:@"/money/ticker"];
-	
-	return [self getJSONFromURL:[[NSURL alloc] initWithString:url]];
-}
-
-+ (NSNumber *) getAvgForCurrency:(NSString *)curr
-{
-	NSDictionary *tickerData = [self getTickerForCurrency:curr];
-	
-	if (tickerData != nil && [tickerData[@"result"] isEqualToString:@"success"]) {
-		NSString *num = tickerData[@"data"][@"avg"][@"value"];
+	if (self->json != nil && [self->json[@"result"] isEqualToString:@"success"]) {
+		NSString *num = self->json[@"data"][@"avg"][@"value"];
 		return [[NSNumber alloc] initWithDouble:[num doubleValue]];
 	}
-	NSLog(@"An error occured while retrieving the data.");
-	/* No data returned */
+	
+	NSLog(@"An error occured while processing the data.");
 	return nil;
 }
 
-#pragma mark - Access to meta data
-
-+ (NSString *) getCurrencySymbol:(NSString *)curr
+- (NSString*)getCurrencySymbol:(NSString*)currency
 {
-	if ([curr isEqualToString:@"USD"]) {
+	if ([currency isEqualToString:@"USD"]) {
 		/* US Dollar */
 		return @"$";
-	} else if ([curr isEqualToString:@"GBP"]) {
+	} else if ([currency isEqualToString:@"GBP"]) {
 		/* Great British Pound */
 		return @"\u00a3";
-	} else if ([curr isEqualToString:@"EUR"]) {
+	} else if ([currency isEqualToString:@"EUR"]) {
 		/* Euro	*/
 		return @"\u20ac";
-	} else if ([curr isEqualToString:@"JPY"]) {
+	} else if ([currency isEqualToString:@"JPY"]) {
 		/* Japanese Yen	*/
 		return @"\u00a5";
-	} else if ([curr isEqualToString:@"AUD"]) {
+	} else if ([currency isEqualToString:@"AUD"]) {
 		/* Australian Dollar */
 		return @"AU$";
-	} else if ([curr isEqualToString:@"CAD"]) {
+	} else if ([currency isEqualToString:@"CAD"]) {
 		/* Canadian Dollar */
 		return @"CA$";
-	} else if ([curr isEqualToString:@"CHF"]) {
+	} else if ([currency isEqualToString:@"CHF"]) {
 		/* Swiss Franc */
 		return @"CHF";
-	} else if ([curr isEqualToString:@"CNY"]) {
+	} else if ([currency isEqualToString:@"CNY"]) {
 		/* Chinese Yuan	*/
 		return @"\u5143";
-	} else if ([curr isEqualToString:@"DKK"]) {
+	} else if ([currency isEqualToString:@"DKK"]) {
 		/* Danish Krone	*/
 		return @"Kr";
-	} else if ([curr isEqualToString:@"HKD"]) {
+	} else if ([currency isEqualToString:@"HKD"]) {
 		/* Hong Kong Dollar	*/
 		return @"HK$";
-	} else if ([curr isEqualToString:@"PLN"]) {
+	} else if ([currency isEqualToString:@"PLN"]) {
 		/* Polish Złoty	*/
 		return @"z\u0142";
-	} else if ([curr isEqualToString:@"RUB"]) {
+	} else if ([currency isEqualToString:@"RUB"]) {
 		/* Russian Rouble */
 		return @"RUB";
-	} else if ([curr isEqualToString:@"SEK"]) {
+	} else if ([currency isEqualToString:@"SEK"]) {
 		/* Swedish Krona */
 		return @"Kr";
-	} else if ([curr isEqualToString:@"SGD"]) {
+	} else if ([currency isEqualToString:@"SGD"]) {
 		/* Singapore Dollar	*/
 		return @"SG$";
-	} else if ([curr isEqualToString:@"THB"]) {
+	} else if ([currency isEqualToString:@"THB"]) {
 		/* Thai Baht */
 		return @"\u0e3f";
-	} else if ([curr isEqualToString:@"NOK"]) {
+	} else if ([currency isEqualToString:@"NOK"]) {
 		/* Norwegian Krone */
 		return @"Kr";
-	} else if ([curr isEqualToString:@"CZK"]) {
+	} else if ([currency isEqualToString:@"CZK"]) {
 		/* Czech Koruna	*/
 		return @"CZK";
 	}
